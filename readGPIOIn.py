220196433d1fe2f3b6cbcbfe5af52a2197623611
Martín Allende <mpcostantini@gmail.com>
2015-02-28 18:38:51 -0300

    BOTH event type catch (instead of FALLING). Move to object design.

--- readGPIOIn.py
+++ readGPIOIn.py
@@ -4,19 +4,42 @@ import RPi.GPIO as GPIO
 import time
 from optparse import OptionParser
 
-parser = OptionParser()
-parser.add_option("-i", "--gpioin", action="store", type="int", dest="gpioin", default=4, help="GPIO number where INPUT device is connected")
-(options, args) = parser.parse_args()
+class GPIOReader:
 
-GPIO.setmode(GPIO.BCM)
-def logInputOff(channel):
-	print('Input turned OFF')
+	@classmethod
+	def init_option_parser(cls):
+		parser = OptionParser()
+		parser.add_option("-i", "--gpioin", action="store", type="int", dest="gpioin", default=4, help="GPIO number where INPUT device is connected")
+		(options, args) = parser.parse_args()
+		return options
 
-GPIO.setup(options.gpioin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(options.gpioin, GPIO.FALLING, callback=logInputOff)
-i = 0
-while True:
-	i = i + 1
-	print(i)
-	time.sleep(1)
-GPIO.cleanup()
+	def __init__(self):
+		self.options = GPIOReader.init_option_parser()    
+		self.init_gpio()
+
+
+	def log_input(self, channel):
+		if GPIO.input(channel) == GPIO.HIGH:
+			print('Input turned ON')
+		else:
+			print('Input turned OFF')
+
+	def init_gpio(self):
+		GPIO.setmode(GPIO.BCM)	
+		GPIO.setup(self.options.gpioin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.add_event_detect(self.options.gpioin, GPIO.BOTH, callback=self.log_input, bouncetime=300)
+
+	def wait_events_loop(self):
+		i = 0
+		while True:
+			i = i + 1
+			print(i)
+			time.sleep(1)	
+
+	def end(self):
+		GPIO.cleanup()
+
+		
+gpio_reader = GPIOReader()
+gpio_reader.wait_events_loop()
+gpio_reader.end()
