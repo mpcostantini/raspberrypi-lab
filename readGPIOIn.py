bcc7e14d8f47c7b5b2b8e80d3f2210e548a4ce63
Martín Allende <mpcostantini@gmail.com>
2015-03-01 23:23:55 -0300

    Write to json file what is read from GPIO

--- readGPIOIn.py
+++ readGPIOIn.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 from optparse import OptionParser
+import json
 
 class GPIOReader:
 
@@ -16,20 +17,26 @@ class GPIOReader:
 	def __init__(self):
 		self.last_event_time = time.time()
 		self.options = GPIOReader.init_option_parser()    
+		self.out_json_file = open('gpio-events.json', 'w')
+		#self.out_json = {'events': []}
 		self.init_gpio()
 
 	def log_input(self, channel):
 		new_event_time = time.time()
+		elapsed_time = new_event_time - self.last_event_time
 		if GPIO.input(channel) == GPIO.HIGH:
-			print 'Input turned ON after {} seconds'.format(new_event_time - self.last_event_time)
+			print 'Input turned ON after {} seconds'.format(elapsed_time)
+			json.dump({'source': 'gpio', 'value': True, 'time' : elapsed_time}, self.out_json_file)
 		else:
-			print 'Input turned OFF after {} seconds'.format(new_event_time - self.last_event_time)
+			print 'Input turned OFF after {} seconds'.format(elapsed_time)
+			json.dump({'source': 'gpio', 'value': False, 'time' : elapsed_time}, self.out_json_file)
 		self.last_event_time = new_event_time
+		self.out_json_file.write('\n')
 
 	def init_gpio(self):
 		GPIO.setmode(GPIO.BCM)	
 		GPIO.setup(self.options.gpioin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.add_event_detect(self.options.gpioin, GPIO.BOTH, callback=self.log_input, bouncetime=300)
+		GPIO.add_event_detect(self.options.gpioin, GPIO.BOTH, callback=self.log_input, bouncetime=200)
 
 	def wait_events_loop(self):
 		i = 0
